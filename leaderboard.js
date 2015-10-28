c108f96f0b0fbcc1cff180404c4de7566af3bfa2
Micah <micahtas@hawaii.edu>
2015-10-27 16:41:48 -1000

    third commit

--- leaderboard.js
+++ leaderboard.js
@@ -44,17 +44,17 @@ if(Meteor.isClient)
 
             'click .increment': function(){
                 var selectedPlayer = Session.get('selectedPlayer');
-                PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+                Meteor.call('modifyPlayerScore', selectedPlayer, 5);
             },
 
             'click .decrement': function(){
                 var selectedPlayer = Session.get('selectedPlayer');
-                PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+                Meteor.call('modifyPlayerScore', selectedPlayer, -5);
             },
 
             'click .remove': function(){
                 var selectedPlayer = Session.get('selectedPlayer');
-                PlayersList.remove(selectedPlayer);
+                Meteor.call('removePlayerData', selectedPlayer);
             }
         }
     );
@@ -64,14 +64,7 @@ if(Meteor.isClient)
             'submit form': function(event){
                 event.preventDefault();
                 var playerNameVar = event.target.playerName.value;
-                var currentUserId = Meteor.userId();
-
-                PlayersList.insert(
-                    {
-                        name: playerNameVar,
-                        score: 0,
-                        createdBy: currentUserId
-                    });
+                Meteor.call('insertPlayerData', playerNameVar);
             }
         }
     );
@@ -84,5 +77,27 @@ if(Meteor.isServer)
     Meteor.publish('thePlayers', function(){
         var currentUserId = this.userId;
         return PlayersList.find({createdBy: currentUserId});
+    });
+
+    Meteor.methods({
+        'insertPlayerData': function(playerNameVar){
+            var currentUserId = Meteor.userId();
+            PlayersList.insert(
+                {
+                    name: playerNameVar,
+                    score: 0,
+                    createdBy: currentUserId
+                });
+        },
+
+        'removePlayerData': function(selectedPlayer){
+            var currentUserId = Meteor.userId();
+            PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+        },
+
+        'modifyPlayerScore': function(selectedPlayer, scoreValue){
+            var currentUserId = Meteor.userId();
+            PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue}});
+        }
     });
 }
